6e7b38b853fe44dbf40759cf41dea8917caaba60
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-10-30 19:58:35 +0530

    Added user Profile Page

---
+++ src/Pages/User/Profile.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import HomeLayout from "../../Layout/HomeLayout";
+import { Link } from "react-router-dom";
+
+function Profile(){
+    const dispatch = useDispatch();
+    const userData = useSelector((state) => state?.auth?.data);
+
+    return(
+       <HomeLayout>
+        <div className="min-h-[90vh] flex items-center justify-center">
+            <div className="my-10 flex flex-col gap-4 rounded-lg p-4 text-white w-80 shadow-[0_0_10px_black]">
+                <img
+                    className="w-40 m-auto rounded-full border border-black"
+                    src={userData?.avatar?.secure_url}
+                />
+                <h3 className="text-xl font-semibold text-center capitalize">
+                    {userData?.fullName}
+                </h3>
+                <div className="grid grid-cols-2">
+                    <p>Email:</p><p>{userData?.email}</p>
+                    <p>Role:</p><p>{userData?.role}</p>
+                    <p>Subscription:</p><p>{userData?.subscription?.status === "active" ? "Active" : "Inactive"}</p>
+                </div>
+                <div className="flex items-center justify-between gap-2">
+                    <Link 
+                        to="/changepassword" 
+                        className="w-1/2 bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 rounded-sm font-semibold py-2 cursor-pointer">
+                            <button className="ml-2">
+                                Change Password
+                            </button>
+                    </Link>
+                    <Link 
+                        to="/user/editprofile" 
+                        className="w-1/2 bg-yellow-500 hover:bg-yellow-600 transition-all ease-in-out duration-300 rounded-sm font-semibold py-2 cursor-pointer">
+                            <button className="ml-2">
+                                Edit Profile
+                            </button>
+                    </Link>
+                </div>
+                {userData?.subscription?.status === "active" &&  (
+                    <button className="w-full bg-red-500 hover:bg-red-600 transition-all ease-in-out duration-300 rounded-sm font-semibold py-2 cursor-pointer">
+                        Cancel Subscription
+                    </button>
+                ) }
+            </div>
+
+        </div>
+
+       </HomeLayout>
+    )
+}
+export default Profile;
