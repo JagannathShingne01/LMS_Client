157197734454cf86948a9d9afbfc0c1f62c8ccdf
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-10-31 18:52:08 +0530

    Added Edit Profile page

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 const initialState = {
     isLoggedIn: localStorage.getItem('isLoggedIn') || false,
     role: localStorage.getItem('role') || "",
-    data: JSON.parse(localStorage.getItem('data')) || {}
+    data: localStorage.getItem('data') !== undefined ? localStorage.getItem('data') : {}
 }
 
 export const createAccount = createAsyncThunk("/auth/signup", async (data)=>{
@@ -26,7 +26,7 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data)=>{
 
 export const login = createAsyncThunk("/auth/login", async (data)=>{
     try {
-        const res = axiosInstance.post("user/login", data)
+        const res = axiosInstance.post("/user/login", data)
         toast.promise(res,{
             loading: "Wait! login in progress....",
             success: (data)=>{
@@ -42,7 +42,7 @@ export const login = createAsyncThunk("/auth/login", async (data)=>{
 
 export const logout = createAsyncThunk("/auth/logout", async () =>{
     try {
-        const res = axiosInstance.get("user/logout")
+        const res = axiosInstance.get("/user/logout")
         toast.promise(res,{
             loading: "Wait! logout in progress....",
             success: (data)=>{
@@ -53,6 +53,34 @@ export const logout = createAsyncThunk("/auth/logout", async () =>{
         return (await res).data
     } catch (error) {
         toast.error(error?.response?.data?.message);
+    }
+}
+)
+
+
+export const updateProfile = createAsyncThunk("/user/update/profile", async (data) =>{
+    try {
+        const res = axiosInstance.put(`/user/update/${data[0]}`, data[1])
+        toast.promise(res,{
+            loading: "Wait! profile update in progress....",
+            success: (data)=>{
+                return data?.data?.message;
+            },
+            error: "Failed to Update Profile, Try Again!"
+        });
+        return (await res).data;
+    } catch (error) {
+        toast.error(error?.response?.data?.message);
+    }
+}
+)
+
+export const getUserData = createAsyncThunk("/user/update/profile", async () =>{
+    try {
+        const res = axiosInstance.get("user/me");
+        return (await res).data
+    } catch (error) {
+        toast.error(error.message);
     }
 }
 )
@@ -76,6 +104,14 @@ const authSlice = createSlice({
             state.data = {};
             state.isLoggedIn = false;
             state.role = "";
+        })
+        .addCase(getUserData.fulfilled,(state, action) => {
+            localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role", action?.payload?.user?.role);
+            state.isLoggedIn = true;
+            state.data = action?.payload?.user;
+            state.role = action?.payload?.user?.role;
         })
     }
 
